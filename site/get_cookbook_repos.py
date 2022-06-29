6c5d03f0eb8ffc9ca4466f029124a08e57c534c6
Julia Kent <46687291+jukent@users.noreply.github.com>
2022-06-29 11:53:50 -0600

    some progress on dumping yaml

--- site/get_cookbook_repos.py
+++ site/get_cookbook_repos.py
@@ -1,8 +1,6 @@
 import requests
 import yaml
 
-url = 'https://api.github.com/users/ProjectPythiaCookbooks/repos'
-
 def _get_cookbook_repo_names(url):
     response = requests.get(url).json()
 
@@ -16,28 +14,32 @@ def _get_cookbook_repo_names(url):
     return repos
 
 
-def _generate_cookbook_gallery_yaml(repos, filepath='cookbook_gallery.yaml'):
-    yaml_dict = []
-    for repo in repos:
-        title = repo.replace('-', ' ').title()
-        cookbook_url = f'https://cookbooks.projectpythia.org/{repo}/README.html'
-        github_url = f'https://github.com/ProjectPythiaCookbooks/{repo}'
-        description = ''
-        names = []
-        thumbnail = f'{github_url}/thumbnail.svg'
-        domains = []
-        packages = []
+#def generate_cookbook_gallery_yaml(repo_url, yaml_filepath='cookbook_gallery.yaml'):
+yaml_filepath='cookbook_gallery.yaml'
+repo_url = 'https://api.github.com/users/ProjectPythiaCookbooks/repos'
+repos = _get_cookbook_repo_names(repo_url)
 
-        authors_dict = {'names':names} #this needs some work
-        tags_dict = {'domains': domains, 'packages': packages}
+yaml_dict = {}
+for repo in repos:
+    title = repo.replace('-', ' ').title()
+    cookbook_url = f'https://cookbooks.projectpythia.org/{repo}/README.html'
+    github_url = f'https://github.com/ProjectPythiaCookbooks/{repo}'
+    description = '' #grab this from github README.md
+    names = [] #grab this from github README.md
+    thumbnail = f'{github_url}/thumbnail.svg'
+    domains = [] #grab this from github README.md
+    packages = [] #grab this from github README.md
 
-        yaml_dict_item = {'title': title, 'repo':repo, 'url': cookbook_url, 'description': description, 'authors': authors_dict, 'thumbnail': thumbnail, 'tags': tags_dict}
-        yaml_dict.append(yaml_dict_item)
+    authors_dict = {'names':names} #this needs some work
+    tags_dict = {'domains': domains, 'packages': packages}
 
-        with open(f"{filepath}", "w") as file:
-            yaml.dump(yaml.load(yaml_dict))
-
-        return
-
+    yaml_dict_item = {'repo':repo, 'url': cookbook_url, 'description': description, 'authors': authors_dict, 'thumbnail': thumbnail, 'tags': tags_dict}
+    yaml_dict[title] = yaml_dict_item
 
 
+with open(yaml_filepath, 'w') as outfile:
+    yaml.dump(yaml_dict, outfile, default_flow_style=False)
+
+
+#url = 'https://api.github.com/users/ProjectPythiaCookbooks/repos'
+#generate_cookbook_gallery_yaml(url)
