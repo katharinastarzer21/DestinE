e2c7c283245ee061028c9a7844e15d770aff1bb9
Julia Kent <46687291+jukent@users.noreply.github.com>
2022-07-05 12:37:50 -0600

    link to issue template

--- site/_extensions/cookbook_gallery_generator.py
+++ site/_extensions/cookbook_gallery_generator.py
@@ -12,7 +12,7 @@ def main(app):
     title = 'Cookbooks Gallery'
     subtext = 'Pythia Cookbooks provide example workflows on more advanced and domain-specific problems developed by the Pythia community. Cookbooks build on top of skills you learn in Pythia Foundations.'
     
-    submit_btn_link = 'https://github.com/ProjectPythiaCookbooks/projectpythiacookbooks.github.io/issues/new?assignees=&labels=resource-gallery-submission&template=update-cookbook.md&title='
+    submit_btn_link = 'https://github.com/ProjectPythiaCookbooks/projectpythiacookbooks.github.io/issues/new?assignees=&labels=content&template=update-cookbook-gallery.md&title=Update+Gallery+with+new+Cookbook'
     submit_btn_txt = 'Submit a new Cookbook'
     menu_html = generate_menu(repo_dicts, submit_btn_txt=submit_btn_txt, submit_btn_link=submit_btn_link)
     
